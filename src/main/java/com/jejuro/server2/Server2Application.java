6fd15238112d3ed6a1a2172723677c2208c8377d
한상우 <sktkddn777@naver.com>
2022-12-08 17:24:57 +0900

    Add: EnableScheduling 어노테이션

--- src/main/java/com/jejuro/server2/Server2Application.java
+++ src/main/java/com/jejuro/server2/Server2Application.java
@@ -2,7 +2,9 @@ package com.jejuro.server2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class Server2Application {
 
